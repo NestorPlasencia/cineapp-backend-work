bffd1f02513684870a2934f4b1bfc23ab2a27e23
NestorPlasencia <nplasencia82@gmail.com>
2020-01-28 20:03:49 -0500

    add Menu CRUD

--- src/main/java/com/mitocode/controller/MenuController.java
+++ src/main/java/com/mitocode/controller/MenuController.java
@@ -1,18 +1,26 @@
 package com.mitocode.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mitocode.exception.ModeloNotFoundException;
 import com.mitocode.model.Menu;
 import com.mitocode.service.IMenuService;
 
@@ -35,6 +43,46 @@ public class MenuController {
 		List<Menu> menues = new ArrayList<>();
 		menues = service.listarMenuPorUsuario(nombre);
 		return new ResponseEntity<List<Menu>>(menues, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Menu> listarPorId(@PathVariable("id") Integer id){
+		Menu obj = service.listarPorId(id);
+		if(obj.getIdMenu() == null) {
+			throw new ModeloNotFoundException("ID NO EXISTE: " + id);
+		}
+		return new ResponseEntity<Menu>(obj, HttpStatus.OK);
+	}
+	
+	/*@PostMapping
+	public ResponseEntity<Menu> registrar(@RequestBody Menu obj) {
+		Menu objeto = service.registrar(obj);
+		return new ResponseEntity<Menu>(objeto, HttpStatus.CREATED);
+	}*/
+	
+	@PostMapping
+	public ResponseEntity<Object> registrar(@Valid @RequestBody Menu obj) {
+		Menu rol = service.registrar(obj);
+		
+		// localhost:8080/menus/2
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(rol.getIdMenu()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+	
+	@PutMapping
+	public ResponseEntity<Menu> modificar(@Valid @RequestBody Menu obj) {
+		Menu objeto = service.modificar(obj);
+		return new ResponseEntity<Menu>(objeto, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> elminar(@PathVariable("id") Integer id){
+		Menu obj = service.listarPorId(id);
+		if(obj.getIdMenu() == null) {
+			throw new ModeloNotFoundException("ID NO EXISTE: " + id);
+		}
+		service.eliminar(id);
+		return new ResponseEntity<Object>(HttpStatus.OK);
 	}
 	
 	
--- src/main/java/com/mitocode/model/Menu.java
+++ src/main/java/com/mitocode/model/Menu.java
@@ -5,26 +5,33 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "menu")
 public class Menu {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idMenu;
 
-	@Column(name = "icono", length = 20)
+	@Size(max = 20, message = "[icono] Máximo 20 caracteres")
+	@Column(name = "icono", nullable = false, length = 20)
 	private String icono;
 
-	@Column(name = "nombre", length = 20)
+	@Size(max = 20, message = "[nombre] Máximo 20 caracteres")
+	@Column(name = "nombre", nullable = false, length = 20)
 	private String nombre;
 
-	@Column(name = "url", length = 50)
+	@Size(max = 50, message = "[url] Máximo 50 caracteres")
+	@Column(name = "url", nullable = false, length = 50)
 	private String url;
 	
 	@ManyToMany(fetch = FetchType.EAGER)
